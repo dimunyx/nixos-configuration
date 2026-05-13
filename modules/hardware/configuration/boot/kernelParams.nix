5ca3fa3a5f82114e786e468943571c9b555606e5
nixos-btw <botnarudumitru10@gmail.com>
2026-05-13 20:12:28 +0300

    Initial NixOS config v13

--- modules/hardware/configuration/boot/kernelParams.nix
+++ modules/hardware/configuration/boot/kernelParams.nix
@@ -4,7 +4,10 @@
 		kernelParams = [
 			"quiet"
 			"splash"
+			"loglevel=3"
+			"systemd.show_status=false"
 			"udev.log_priority=3"
+			"rd.udev.log_lovel=3"
 			"rd.systemd.show_status=false"
 		];
 	};
