88918ce41fe29cfbafa57757933c120d1d6d065e
nixos-btw <botnarudumitru10@gmail.com>
2026-05-15 19:51:04 +0300

    Initial NixOS config v30

--- modules/home-manager/users/botnaru/programs/gammastep/home.nix
+++ modules/home-manager/users/botnaru/programs/gammastep/home.nix
@@ -6,18 +6,18 @@
 			tray = true;
 			settings = {
 				general = {
-					dawn-time = 07:30;
-					dusk-time = 20:00;
-					temp-night = 3500;
-					brightness-day = 1.0;
-					brightness-night = 1.0;
-					fade = 1;
+					dawn-time = "07:30";
+					dusk-time = "20:00";
+					temp-night = "3500";
+					brightness-day = "1.0";
+					brightness-night = "1.0";
+					fade = "1";
 					adjustment-method = "wayland";
 					location-provider = "manual";
 				};
 				manual = {
-					lat = 47.0;
-					lon = 28.8;
+					lat = "47.0";
+					lon = "28.8";
 				};
 			};
 		};
