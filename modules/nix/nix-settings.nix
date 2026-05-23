d613ec3a1405323d1fc5e1438a47aa4b5a68ee4b
nixos-btw <botnarudumitru10@gmail.com>
2026-05-23 18:56:52 +0300

    Switching to linux-cachyos kernel! v2

--- modules/nix/nix-settings.nix
+++ modules/nix/nix-settings.nix
@@ -7,11 +7,13 @@
 				"nix-command"
 				"flakes"
 			];
-			substituers = [
+			substituters = [
 				"https://attic.xuyh0120.win/lantian"
+				"https://cache.garnix.io"
 			];
 			trusted-public-keys = [
 				"lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+				"cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
 			];
 		};
 	};
