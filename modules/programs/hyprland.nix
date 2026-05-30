6c1a19d920ec05f88675d38e5cfdaf64c187ec51
nixos-btw <botnarudumitru10@gmail.com>
2026-05-30 21:34:02 +0300

    main

--- modules/programs/hyprland.nix
+++ modules/programs/hyprland.nix
@@ -5,6 +5,7 @@
 			enable = true;
 			package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
 			portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+			withUWSM = false;
 			xwayland = {
 				enable = true;
 			};
