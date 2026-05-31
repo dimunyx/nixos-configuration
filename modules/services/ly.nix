d550ba91b807524efa15dff668e65fa70bd7d163
nixos-btw <botnarudumitru10@gmail.com>
2026-05-31 10:40:01 +0300

    main

--- modules/services/ly.nix
+++ modules/services/ly.nix
@@ -1,9 +1,11 @@
 { config, lib, pkgs, pkgs-unstable, ... }:
 {
 	services = {
-		ly = {
-			enable = true;
-			package = pkgs-unstable.ly;
+		displayManager = {
+			ly = {
+				enable = true;
+				package = pkgs-unstable.ly;
+			};
 		};
 	};
 }
